d95d295751a4f70abe76ed934797b2c4ab94d7ea
William Murphy <wmurphy@repay.com>
2020-11-12 18:48:02 -0600

    no message

--- lib/connection_manager/core.rb
+++ lib/connection_manager/core.rb
@@ -1,19 +1,9 @@
 module ConnectionManager
   module Core
     # We want to make sure we get the full table name with schema
-    def arel_table_with_check_name # :nodoc:
+    def arel_table # :nodoc:
       @arel_table = Arel::Table.new(table_name, arel_engine) unless (@arel_table && (@arel_table.name == self.table_name))
       @arel_table
-    end
-
-    def self.included(base)
-      base.alias_method :arel_table, :check_name
-    end
-
-    def self.extended(base)
-      class << base
-        self.alias_method :arel_table, :check_name
-      end
     end
   end
 end
